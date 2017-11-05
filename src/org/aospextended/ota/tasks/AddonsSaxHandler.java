cea98897c0a9bc49a36927e17557c8c9fd83f628
Kukun Mishra <kukun7610@gmail.com>
2017-11-05 20:28:49 +0530

    DesiOTA Bring up

--- src/org/aospextended/ota/tasks/AddonsSaxHandler.java
+++ src/org/aospextended/ota/tasks/AddonsSaxHandler.java
@@ -1,4 +1,4 @@
-package org.aospextended.ota.tasks;
+package org.dosp.ota.tasks;
 
 import java.util.ArrayList;
 
@@ -8,8 +8,8 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import android.util.Log;
 
-import org.aospextended.ota.Addon;
-import org.aospextended.ota.utils.Constants;
+import org.dosp.ota.Addon;
+import org.dosp.ota.utils.Constants;
 
 public class AddonsSaxHandler extends DefaultHandler implements Constants {
 	
